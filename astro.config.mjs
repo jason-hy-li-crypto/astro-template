c7e8f854d31f6bf95f27c3561fe48ec721fd4fee
jason.hyli <jason.li@cronoslabs.org>
2024-05-31 15:53:37 +1000

    sourcemap debug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-import paraglide from '@inlang/paraglide-astro'
+import paraglide from "@inlang/paraglide-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch:true, // able prefetch page on link hover
-  output:'static',
+  prefetch: true, // able prefetch page on link hover
+  output: "static",
   i18n: {
     defaultLocale: "en", // the default locale
     locales: ["en", "ko"], // the locales you want to support
+  },
+  vite: {
+    build: {
+      sourcemap: "inline",
+    },
   },
   prefetch: true,
   integrations: [
